99a8525dd4377779bc1087b7e5582427cab74203
Leonardo Gabriel Leenen <leonardo.leenen@agtech.com.ar>
2019-09-12 01:40:23 -0300

    Visual BugFix

--- webapp/pages/dashboard/no_events.tsx
+++ webapp/pages/dashboard/no_events.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import NoEventSVG from '../../static/svg/noEvents.svg';
 import BottomBar from '../../components/bottomBar';
+import Arrow from '../../static/svg/ChalkArrowBlack.svg';
 
 import '../../styles/index.scss';
 
@@ -14,11 +15,20 @@ class NoEvent extends React.Component {
   render() {
     return <div className="noEvents bg-scroll h-screen">
       <label>No events for now</label>
-      <img src={NoEventSVG}/>
+      <img className="avatarNoEvent" src={NoEventSVG}/>
       <article>
         <p>There are not event for the moment, but don't rush, stay hungry and keep moving. Allways betters things are coming soon :-) </p>
         <p>May be you want to make a reservation. In this case click here</p>
       </article>
+      <div style={{display:"grid", gridTemplateColumns:"repeat(6,1fr)"}}>
+        <span/>
+        <span/>
+        <img className="arrow" src={Arrow}></img>
+        <span/>
+        <span/>
+        <span/>
+
+      </div>
       <BottomBar/>
     </div>
   }
