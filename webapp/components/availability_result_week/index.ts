bb27eff71e597ce586e1983d2252bb735ac58d2b
Maria noel Leenen <marianoel.leenen@agtech.com.ar>
2019-09-27 17:40:21 -0300

    availability

---
+++ webapp/components/availability_result_week/index.ts
@@ -0,0 +1,12 @@
+import React from 'react'
+import {view} from './view'
+
+class AvailabilityResultWeek extends React.Component {
+  constructor(props) {
+    super(props)
+    
+  }
+  render = () => view(this.props)
+}
+
+export default AvailabilityResultWeek
