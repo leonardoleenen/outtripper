2055bc42bfc8c027f6247be039b1a4623fb2fddd
Leonardo Gabriel Leenen <leonardo.leenen@agtech.com.ar>
2019-10-01 03:05:19 -0300

    TODO: GroupBy ProgramID

--- webapp/redux/actions/reservation.ts
+++ webapp/redux/actions/reservation.ts
@@ -1,6 +1,7 @@
 import { SET_MONTH_FILTER } from '../reducers/reservation';
 import { businessService, BusinessService } from '../../services/index'
 import {IDateAvailable} from '../../services/type'
+import _ from 'underscore'
 
 const ds: BusinessService = businessService
 
@@ -8,6 +9,9 @@ const ds: BusinessService = businessService
 export const setMonthFilter = (month: number) => {
   return (dispatch, getState) => {
     ds.getDatesAvailabilityList().then( (result: IDateAvailable[]) => {
+      
+      // TODO: GroupBy program_id
+      // console.log(_.groupBy(result, (e:IDateAvailable) => e.program_id ))
       return dispatch({
         type: SET_MONTH_FILTER,
         monthSelected: month,
