18ea757c08cff14ede3e5a7acf268c0607fc01e6
Juan Antenucci <juan@cafebinario.com>
2019-11-01 16:27:14 -0300

    Itinerary flow

--- webapp/pages/client_view/mytrips/itinerary.tsx
+++ webapp/pages/client_view/mytrips/itinerary.tsx
@@ -1,6 +1,22 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
+import axios from 'axios';
+import InternationalFlightCard from '../../../components/itinerary/card_international_flight'
+import Loading from '../../../components/Loading'
+import {FlightData} from '../../../components/itinerary/card_international_flight'
 
 export default () => {
+    const [ flightList, setFlightList] = useState([])
+    useEffect( () => {
+        const fetchFlights = async () => {
+            const resolve = await axios.get('https://api.myjson.com/bins/q6kno') as any
+            setFlightList(resolve.data)
+        }
+        fetchFlights()
+    }, [])
 
-    return <div>Itinerary</div>
+    if (flightList.length == 0) return <Loading></Loading>
+    
+    return <div>
+        {flightList.map((flight: FlightData) => (<InternationalFlightCard key={flight.flight_number} label="Flight to Buenos Aires" flight_data={flight}></InternationalFlightCard>))}
+    </div>
 }
