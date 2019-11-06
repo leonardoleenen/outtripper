1b9e5905314fb247fd7bfa6e50e76e8fc63868e0
Leonardo Gabriel Leenen <leonardo.leenen@agtech.com.ar>
2019-11-05 22:28:23 -0300

    BugFix my trips

--- webapp/pages/client_view/mytrips/itinerary.tsx
+++ webapp/pages/client_view/mytrips/itinerary.tsx
@@ -7,11 +7,25 @@ import BottomBar from '../../../components/my_trips/bottom_bar'
 import PreTripInfo from '../../../components/my_trips/pre_trip_info'
 import Loading from '../../../components/Loading'
 import {FlightData} from '../../../components/my_trips/itinerary/card_international_flight'
+import moment from 'moment';
 
 export interface Itinerary {
     date: string
     items: Array<string | number>
   }
+
+const renderCard = (item: any) => {
+
+  // TODO: Complete with all pobilitities
+  
+  switch (item.type){
+    case 'flight':
+      return <InternationalFlightCard key={item.code} label="Flight to Buenos Aires" flight_data={item}></InternationalFlightCard>
+    default: 
+      return <p> Definilo puto! {item.type}</p>
+  }
+  
+}
 
 export default () => {
   const bs = businessService
@@ -31,11 +45,19 @@ export default () => {
 
   if (itinerary.length == 0) return <Loading></Loading>
   
+  //<InternationalFlightCard key={flight.code} label="Flight to Buenos Aires" flight_data={flight}></InternationalFlightCard>
+  debugger
   return <div>
       <ProgramHeader program={program}></ProgramHeader>
       <NavBar></NavBar>
       <PreTripInfo></PreTripInfo>
-      {itinerary.items.map((flight: FlightData) => (<InternationalFlightCard key={flight.code} label="Flight to Buenos Aires" flight_data={flight}></InternationalFlightCard>))}
+      { itinerary.map((e: any) => ( // First map group by date 
+        <div>
+          <p>{moment(e.date).format('Do MMM YYYY')}</p> 
+          { e.items.map( (e:any) => { // Second map iterate all elements 
+            return renderCard(e) // Call a function for render
+          })}
+      </div>))}
       <BottomBar></BottomBar>
   </div>
 }
