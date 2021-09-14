9ec73dfa324f042fefa10f6b205b822fbd73f26f
Leonardo Leenen <leonardoleenen@gmail.com>
2021-09-14 19:13:58 -0300

    Implements daisy

--- components/Configure/Services/BarMenu/index.tsx
+++ components/Configure/Services/BarMenu/index.tsx
@@ -2,12 +2,75 @@ import React from 'react'
 
 export default (): JSX.Element => {
     return (
-        <div className="bg-blue-100" style={{ width: '20em' }}>
-            <div>menu 1</div>
-            <div>menu 1</div>
-            <div>menu 1</div>
-            <div>menu 1</div>
-            <div>menu 1</div>
+        <div className="p-4 ">
+            <select className="select select-bordered w-full max-w-xs my-4">
+                <option>telekinesis</option>
+                <option>time travel</option>
+                <option>invisibility</option>
+            </select>
+            <ul className="menu py-4 ">
+                <li className="bordered disabled">
+                    <a>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            className="inline-block w-5 h-5 mr-2 stroke-current"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"
+                            ></path>
+                        </svg>
+                        Item is disabled
+                    </a>
+                </li>
+                <li className="bordered">
+                    <a>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            className="inline-block w-5 h-5 mr-2 stroke-current"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"
+                            ></path>
+                        </svg>
+                        It has border
+                    </a>
+                </li>
+                <li className="hover-bordered">
+                    <a>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            className="inline-block w-5 h-5 mr-2 stroke-current"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"
+                            ></path>
+                        </svg>
+                        It shows border on hover
+                    </a>
+                </li>
+            </ul>
+            <button className="btn btn-success w-full bottom-0">
+                Create New Service
+            </button>
+
+            <button className="btn btn-error w-full bottom-0 mt-4">
+                Remove
+            </button>
         </div>
     )
 }
--- pages/configure/services/index.tsx
+++ pages/configure/services/index.tsx
@@ -7,12 +7,28 @@ import BarMenu from '../../../components/Configure/Services/BarMenu'
 
 const ConfigureServices: NextPage = () => {
     return (
-        <div>
+        <div className="p-4">
             <Header />
             <Container className="flex justify-between w-full">
                 <div className="flex w-3/4">
                     <BarMenu />
-                    <main className="bg-red-300 w-full">maestro</main>
+                    <article className=" prose lg:prose-xl w-full pt-8 px-4">
+                        <h1>
+                            Garlic bread with cheese: What the science tells us
+                        </h1>
+                        <p>
+                            For years parents have espoused the health benefits
+                            of eating garlic bread with cheese to their
+                            children, with the food earning such an iconic
+                            status in our culture that kids will often dress up
+                            as warm, cheesy loaf for Halloween.
+                        </p>
+                        <p>
+                            But a recent study shows that the celebrated
+                            appetizer may be linked to a series of rabies cases
+                            springing up around the country.
+                        </p>
+                    </article>
                 </div>
                 <PreviewPage />
             </Container>
