bc501604bfcaa509480ac02479faafc67c897a55
leonardoleenen <leonardo@flicktrip.com>
2021-09-16 23:35:43 +0000

    Adding new fields

--- components/eVoucher/Contact/index.tsx
+++ components/eVoucher/Contact/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { UIEVoucher } from '../../../stores/evoucher.store'
 
+
 export default (): JSX.Element => {
     const voucher = UIEVoucher.useState(s => s)
 
@@ -8,10 +9,21 @@ export default (): JSX.Element => {
         <div className="prose">
             <h3>Do you have any inquire?</h3>
             <div className="bg-base-200 font-thin  rounded-lg">
-                <div className="m-4">
-                    <p>For Emergency: {voucher.emergencyPhone}</p>
-                </div>
+                <p className="p-4">For Emergency: {voucher.emergencyPhone}</p>
             </div>
+
+            <div className="bg-base-200 font-thin  rounded-lg ">
+                <p className="p-4">Contact Phone: {voucher.contactPhone}</p>
+            </div>
+
+            <div className="bg-base-200 font-thin  rounded-lg ">
+                <p className="p-4">Operator Phone: {voucher.operatorPhone}</p>
+            </div>
+            <div className="bg-base-200 font-thin  rounded-lg ">
+                <p className="p-4">Email: {voucher.email}</p>
+            </div>
+
+
         </div>
     )
 }
